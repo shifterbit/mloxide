db079722cc7ce0d48dbe790100ef2c36b17c9089
shifterbit <48891590+shifterbit@users.noreply.github.com>
2024-04-27 19:16:51 -0400

    add support for typechecking equality expressions

--- src/type_checker.rs
+++ src/type_checker.rs
@@ -4,6 +4,7 @@ use crate::ast::{AstNode, Operator};
 pub enum Type {
     Int,
     Real,
+    Bool,
     Unknown,
 }
 
@@ -29,7 +30,11 @@ impl TypedAstNode {
         match self {
             TypedAstNode::Int(_) => Type::Int,
             TypedAstNode::Real(_) => Type::Real,
-            TypedAstNode::Unary { node_type: _, op: _, expr } => {
+            TypedAstNode::Unary {
+                node_type: _,
+                op: _,
+                expr,
+            } => {
                 let expr_type = expr.get_type();
                 return expr_type;
             }
@@ -58,14 +63,13 @@ pub fn typecheck(node: Box<AstNode>) -> TypedAstNode {
         AstNode::Binary { op, lhs, rhs } => {
             let typed_lhs = typecheck(lhs);
             let typed_rhs = typecheck(rhs);
-
             let l_type = typed_lhs.get_type();
             let r_type = typed_rhs.get_type();
+            let (l_expected, r_expected) = allowed_binary_op_type(op, l_type.clone());
 
-            let full_type = if l_type == r_type && l_type != Type::Unknown {
-                l_type
+            let full_type = if (&l_expected, &r_expected) == (&l_type, &r_type) {
+                binary_return_type(op, l_type, r_type)
             } else {
-                println!("Expected types ({l_type:#?} {op:?} {l_type:#?}) got ({l_type:#?} {op:?} {r_type:#?})");
                 Type::Unknown
             };
 
@@ -79,7 +83,78 @@ pub fn typecheck(node: Box<AstNode>) -> TypedAstNode {
         AstNode::Unary { op, expr } => {
             let typed_expr = typecheck(expr);
             let e_type = typed_expr.get_type();
-            return TypedAstNode::Unary { node_type: e_type, op, expr: Box::new(typed_expr) };
+            let expected_type = allowed_infix_op_type(op, e_type.clone());
+            return TypedAstNode::Unary {
+                node_type: expected_type,
+                op,
+                expr: Box::new(typed_expr),
+            };
         }
+    }
+}
+
+fn binary_return_type(operator: Operator, left_type: Type, right_type: Type) -> Type {
+    match operator {
+        Operator::Plus | Operator::Minus | Operator::Negation | Operator::Multiply => {
+            let allowed_types = operator_return_types(operator);
+            let left_valid = allowed_types.contains(&left_type);
+            let right_valid = allowed_types.contains(&right_type);
+            let both_valid = left_valid && right_valid;
+            if allowed_types.contains(&left_type) && both_valid {
+                return left_type;
+            } else {
+                return Type::Unknown;
+            }
+        }
+        Operator::Equal | Operator::NotEqual => return Type::Bool,
+        Operator::IntDivision => return Type::Int,
+        Operator::RealDivision => return Type::Real,
+        _ => return Type::Unknown,
+    }
+}
+
+fn operator_return_types(operator: Operator) -> Vec<Type> {
+    match operator {
+        Operator::Plus | Operator::Minus | Operator::Negation | Operator::Multiply => {
+            return vec![Type::Int, Type::Real];
+        }
+        Operator::Equal | Operator::NotEqual => {
+            return vec![Type::Bool];
+        }
+        Operator::IntDivision => {
+            return vec![Type::Int];
+        }
+        Operator::RealDivision => {
+            return vec![Type::Real];
+        }
+    }
+}
+fn allowed_binary_op_type(operator: Operator, left_type: Type) -> (Type, Type) {
+    match operator {
+        Operator::Plus | Operator::Minus | Operator::Multiply => match left_type {
+            Type::Int => return (Type::Int, Type::Int),
+            Type::Real => return (Type::Real, Type::Real),
+            _ => panic!("Invalid Type"),
+        },
+        Operator::Equal | Operator::NotEqual => match left_type {
+            t => return (t.clone(), t),
+        },
+        Operator::IntDivision => return (Type::Int, Type::Int),
+        Operator::RealDivision => return (Type::Real, Type::Real),
+        _ => panic!("Expected Binary Operator"),
+    }
+}
+
+fn allowed_infix_op_type(operator: Operator, expr_type: Type) -> Type {
+    match operator {
+        Operator::Negation => {
+            let allowed_types = vec![Type::Int, Type::Real];
+            if allowed_types.contains(&expr_type) {
+                return expr_type;
+            } else {
+                return Type::Unknown;
+            }
+        }
+        _ => Type::Unknown,
     }
 }
