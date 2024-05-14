d348a6b6a774709c7c12f3b183ed8135572819f2
shifterbit <48891590+shifterbit@users.noreply.github.com>
2024-05-13 22:19:58 -0400

    Implement basic error reporting for syntax errors

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,13 @@ use std::{env, fs};
 
 use parser::parse;
 
-use crate::{interpreter::eval_expression, type_checker::typecheck};
+use crate::{interpreter::eval_expression, parser::stringify_parse_errors, type_checker::typecheck};
 mod ast;
-mod token;
+mod interpreter;
 mod lexer;
 mod parser;
+mod token;
 mod type_checker;
-mod interpreter;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -17,9 +17,20 @@ fn main() {
     println!("Plaintext:\n {:#?}", &text);
     let mut lexer = lexer::Lexer::new(&text.unwrap());
     let ast = parse(&mut lexer);
-    println!("AST:\n {:#?}", ast);
-    let tast = typecheck(ast);
-    println!("Typed AST:\n {:#?}", tast);
-    let eval = eval_expression(tast);
-    println!("Eval Result:\n {:#?}", eval);
+    match ast {
+        Ok(a) => {
+            println!("AST:\n {:#?}", a);
+            let tast = typecheck(a);
+            println!("Typed AST:\n {:#?}", tast);
+            let eval = eval_expression(tast);
+            println!("Eval Result:\n {:#?}", eval);
+        }
+        Err(e) => {
+            let errors = stringify_parse_errors(e);
+            for error in errors {
+                println!("{}", error);
+            }
+        }
+    }
+
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,126 +1,227 @@
+use std::fmt::{self, Display};
+
 use crate::ast::{AstNode, Operator};
 use crate::lexer::Lexer;
-use crate::token::TokenType;
+use crate::token::{Token, TokenType};
 
-pub fn parse(lexer: &mut Lexer) -> AstNode {
-    //      parse_expr(lexer, 0)
-    expression(lexer)
+type ParseErrorList = Vec<ParseError>;
+
+#[derive(Debug, Clone)]
+pub struct ParseError {
+    message: String,
+    token: Token,
 }
-fn expression(lexer: &mut Lexer) -> AstNode {
+
+impl ParseError {
+    fn new(message: &str, token: Token) -> ParseError {
+        ParseError {
+            message: message.to_string(),
+            token,
+        }
+    }
+}
+
+pub fn stringify_parse_errors(errors: ParseErrorList) -> Vec<String> {
+    let mut messages: Vec<String> = Vec::new();
+    for error in errors {
+        messages.push(error.to_string());
+    }
+    messages
+}
+
+impl Display for ParseError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let position = self.token.position;
+        write!(f, "{position} ParseError: {}", self.message)
+    }
+}
+
+pub fn parse(lexer: &mut Lexer) -> Result<AstNode, Vec<ParseError>> {
+    //      parse_expr(lexer, 0)
+    let mut errors: Vec<ParseError> = Vec::new();
+    expression(lexer, &mut errors)
+}
+fn expression(lexer: &mut Lexer, errors: &mut Vec<ParseError>) -> Result<AstNode, ParseErrorList> {
     let curr = lexer.peek();
     let token_type = curr.token_type;
     match token_type {
-        TokenType::If => if_expression(lexer),
-        _ => equality(lexer),
+        TokenType::If => if_expression(lexer, errors),
+        TokenType::Eof => Err(errors.clone()),
+        _ => equality(lexer, errors),
     }
 }
 
-fn if_expression(lexer: &mut Lexer) -> AstNode {
+fn if_expression(
+    lexer: &mut Lexer,
+    errors: &mut Vec<ParseError>,
+) -> Result<AstNode, ParseErrorList> {
     let if_tok = lexer.next();
     if if_tok.token_type != TokenType::If {
-        panic!("Expected If Token")
+        let error_val = ParseError::new("expected token if", if_tok);
+        errors.push(error_val);
+        return Err(errors.clone());
     }
-    let condition = expression(lexer);
+    let condition = expression(lexer, errors);
+    let condition_expr: AstNode = match condition {
+        Ok(expr) => expr,
+        Err(errors) => return Err(errors.clone()),
+    };
+
     let then_tok = lexer.next();
     if then_tok.token_type != TokenType::Then {
-        panic!("Expected Then Token")
+        let error_val = ParseError::new("expected token then", then_tok);
+        errors.push(error_val);
+        return Err(errors.clone());
     }
-    let if_body = expression(lexer);
+
+    let if_body = expression(lexer, errors);
+    let if_expr: AstNode = match if_body {
+        Ok(expr) => expr,
+        Err(errors) => return Err(errors),
+    };
+
     let else_tok = lexer.next();
     if else_tok.token_type != TokenType::Else {
-        panic!("Expected Else Token")
+        let error_val = ParseError::new("expected token else", else_tok);
+        errors.push(error_val);
+        return Err(errors.clone());
     }
-    let else_body = expression(lexer);
+    let else_body = expression(lexer, errors);
+    let else_expr: AstNode = match else_body {
+        Ok(expr) => expr,
+        Err(errors) => return Err(errors),
+    };
 
-    AstNode::If {
-        condition: Box::new(condition),
-        if_body: Box::new(if_body),
-        else_body: Box::new(else_body),
-    }
+    Ok(AstNode::If {
+        condition: Box::new(condition_expr),
+        if_body: Box::new(if_expr),
+        else_body: Box::new(else_expr),
+    })
 }
 
-fn equality(lexer: &mut Lexer) -> AstNode {
-    let expr = term(lexer);
+fn equality(lexer: &mut Lexer, errors: &mut Vec<ParseError>) -> Result<AstNode, ParseErrorList> {
+    let expr = term(lexer, errors);
+    let lhs: AstNode = match expr {
+        Ok(e) => e,
+        Err(errs) => return Err(errs),
+    };
+
     let token = lexer.peek();
     match token.token_type {
         TokenType::EqualEqual | TokenType::NotEqual => {
             lexer.next();
-            let right = equality(lexer);
-            AstNode::Binary {
+            let right = equality(lexer, errors);
+            let rhs: AstNode = match right {
+                Ok(e) => e,
+                Err(e) => return Err(e),
+            };
+
+            Ok(AstNode::Binary {
                 op: get_operator(token.token_type),
-                lhs: Box::new(expr),
-                rhs: Box::new(right),
-            }
+                lhs: Box::new(lhs),
+                rhs: Box::new(rhs),
+            })
         }
-        _ => expr,
+        _ => Ok(lhs),
     }
 }
 
-fn term(lexer: &mut Lexer) -> AstNode {
-    let expr = factor(lexer);
+fn term(lexer: &mut Lexer, errors: &mut Vec<ParseError>) -> Result<AstNode, ParseErrorList> {
+    let expr = factor(lexer, errors);
+    let lhs: AstNode = match expr {
+        Ok(e) => e,
+        Err(e) => return Err(e),
+    };
+
     let token = lexer.peek();
     match token.token_type {
         TokenType::Minus | TokenType::Plus => {
             lexer.next();
-            let right = term(lexer);
-            AstNode::Binary {
+            let right = term(lexer, errors);
+            let rhs: AstNode = match right {
+                Ok(e) => e,
+                Err(e) => return Err(e),
+            };
+
+            Ok(AstNode::Binary {
                 op: get_operator(token.token_type),
-                lhs: Box::new(expr),
-                rhs: Box::new(right),
-            }
+                lhs: Box::new(lhs),
+                rhs: Box::new(rhs),
+            })
         }
-        _ => expr,
+        _ => Ok(lhs),
     }
 }
 
-fn factor(lexer: &mut Lexer) -> AstNode {
-    let expr = unary(lexer);
+fn factor(lexer: &mut Lexer, errors: &mut Vec<ParseError>) -> Result<AstNode, ParseErrorList> {
+    let expr = unary(lexer, errors);
+    let lhs: AstNode = match expr {
+        Ok(e) => e,
+        Err(e) => return Err(e),
+    };
+
     let token = lexer.peek();
     match token.token_type {
         TokenType::ForwardSlash | TokenType::Star => {
             lexer.next();
-            let right = factor(lexer);
-            AstNode::Binary {
+            let right = factor(lexer, errors);
+            let rhs: AstNode = match right {
+                Ok(e) => e,
+                Err(e) => return Err(e),
+            };
+            Ok(AstNode::Binary {
                 op: get_operator(token.token_type),
-                lhs: Box::new(expr),
-                rhs: Box::new(right),
-            }
+                lhs: Box::new(lhs),
+                rhs: Box::new(rhs),
+            })
         }
-        _ => expr,
+        _ => Ok(lhs),
     }
 }
 
-fn unary(lexer: &mut Lexer) -> AstNode {
+fn unary(lexer: &mut Lexer, errors: &mut Vec<ParseError>) -> Result<AstNode, ParseErrorList> {
     let token = lexer.peek();
     match token.token_type {
         TokenType::Negation => {
             lexer.next();
-            let expr = unary(lexer);
-            AstNode::Unary {
+            let expr = unary(lexer, errors);
+            let right_expr: AstNode = match expr {
+                Ok(e) => e,
+                Err(e) => return Err(e),
+            };
+
+            Ok(AstNode::Unary {
                 op: get_operator(token.token_type),
-                expr: Box::new(expr),
-            }
+                expr: Box::new(right_expr),
+            })
         }
-        _ => primary(lexer),
+        _ => primary(lexer, errors),
     }
 }
 
-fn primary(lexer: &mut Lexer) -> AstNode {
+fn primary(lexer: &mut Lexer, errors: &mut Vec<ParseError>) -> Result<AstNode, ParseErrorList> {
     let token = lexer.next();
     match token.token_type {
-        TokenType::Int(n) => AstNode::Int(n),
-        TokenType::Float(n) => AstNode::Float(n),
-        TokenType::Bool(b) => AstNode::Bool(b),
-        TokenType::Identifier(i) => AstNode::Identifier(i),
+        TokenType::Int(n) => Ok(AstNode::Int(n)),
+        TokenType::Float(n) => Ok(AstNode::Float(n)),
+        TokenType::Bool(b) => Ok(AstNode::Bool(b)),
+        TokenType::Identifier(i) => Ok(AstNode::Identifier(i)),
         TokenType::LeftParen => {
-            let expr = expression(lexer);
+            let expr = expression(lexer, errors);
+            let inner_expr: AstNode = match expr {
+                Ok(e) => e,
+                Err(e) => return Err(e),
+            };
+
             if lexer.next().token_type == TokenType::RightParen {
-                AstNode::Grouping(Box::new(expr))
+                Ok(AstNode::Grouping(Box::new(inner_expr)))
             } else {
-                panic!("Expected ) after expression")
+                let error = ParseError::new("Expected closing parenthesis", lexer.peek());
+                errors.push(error);
+                return Err(errors.clone())
             }
-        },
-        _ =>  panic!("Expected literal value")
+        }
+        _ => panic!("Expected literal value"),
     }
 }
 
