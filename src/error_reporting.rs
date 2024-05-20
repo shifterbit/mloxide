dea802093988af96247eefb602d10894688d45d6
shifterbit <48891590+shifterbit@users.noreply.github.com>
2024-05-19 22:12:02 -0400

    basic single file error reporting display with codespan-reporting

---
+++ src/error_reporting.rs
@@ -0,0 +1,24 @@
+use codespan_reporting::diagnostic::Label;
+use codespan_reporting::term;
+use codespan_reporting::term::termcolor::{ColorChoice, StandardStream};
+use codespan_reporting::{diagnostic::Diagnostic, files::SimpleFile};
+
+use crate::parser::ParseErrorList;
+
+pub fn errors_from_file(filename: &str, text: &str, errors: ParseErrorList) {
+    let writer = StandardStream::stderr(ColorChoice::Auto);
+    let config = codespan_reporting::term::Config::default();
+
+    let file = SimpleFile::new(filename, text);
+
+    for error in errors {
+        let start = error.start();
+        let end = error.end();
+        let diagnostic: Diagnostic<()> = Diagnostic::error()
+            .with_message(error.to_string())
+            .with_code("ParseError")
+            .with_labels(vec![Label::primary((), start..end)]);
+
+        let _emit = term::emit(&mut writer.lock(), &config, &file, &diagnostic);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,7 @@ use std::{env, fs};
 use parser::parse;
 
 use crate::{
-    ast::AstNode,
-    interpreter::{eval_expression, Value},
-    name_resolution::{resolve_symbols, SymbolTable},
-    parser::stringify_parse_errors,
-    type_checker::{typecheck, Type},
+    ast::AstNode, error_reporting::errors_from_file, interpreter::{eval_expression, Value}, name_resolution::{resolve_symbols, SymbolTable}, parser::stringify_parse_errors, type_checker::{typecheck, Type}
 };
 mod ast;
 mod interpreter;
@@ -16,13 +12,14 @@ mod name_resolution;
 mod parser;
 mod token;
 mod type_checker;
+mod error_reporting;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let path = &args[1];
-    let text = fs::read_to_string(path);
+    let text = fs::read_to_string(path).unwrap();
     println!("Plaintext:\n {:#?}", &text);
-    let mut lexer = lexer::Lexer::new(&text.unwrap());
+    let mut lexer = lexer::Lexer::new(&text);
     let ast = parse(&mut lexer);
     match ast {
         Ok(a) => {
@@ -38,10 +35,7 @@ fn main() {
             println!("{:?}", eval);
         }
         Err(e) => {
-            let errors = stringify_parse_errors(e);
-            for error in errors {
-                println!("{}", error);
-            }
+            errors_from_file(path, &text, e.clone());
         }
     }
 }
